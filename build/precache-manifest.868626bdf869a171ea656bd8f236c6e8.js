64331d71173b0af5de50fa231b2af294a526f721
Pranav <101118335+pranavchachra1702@users.noreply.github.com>
2022-07-09 21:43:05 +0530

    Netlify Deploy-Build File

---
+++ build/precache-manifest.868626bdf869a171ea656bd8f236c6e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a37e0a998a3b7b3c31a82a255e0f79f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b94891f463b927eb426f",
+    "url": "/static/css/main.35c3b84a.chunk.css"
+  },
+  {
+    "revision": "4ca1370c001a0201b4db",
+    "url": "/static/js/2.d9b8a4a2.chunk.js"
+  },
+  {
+    "revision": "b94891f463b927eb426f",
+    "url": "/static/js/main.7eea52a1.chunk.js"
+  },
+  {
+    "revision": "db52d994a86efef5cabd",
+    "url": "/static/js/runtime-main.3d514ccf.js"
+  }
+]);
